00d2deedc5ecece7e410d533a21e232a6b549951
Ibrahim <abram.pomposelli1@gmail.com>
2021-07-26 03:13:50 +0200

    configure Jest, Cypress, RTL

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "plugin:prettier/recommended"],
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:testing-library/react",
+    "plugin:testing-library/dom",
+    "plugin:jest-dom/recommended",
+    "next",
+    "next/core-web-vitals",
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,7 +20,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "jest-dom", "testing-library"],
   rules: {
     "prettier/prettier": "error",
     "no-unused-vars": "warn",
@@ -22,5 +31,20 @@ module.exports = {
     "class-methods-use-this": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/jsx-one-expression-per-line": "off",
+    "testing-library/no-render-in-setup": "error",
+    "testing-library/no-wait-for-empty-callback": "error",
+    "testing-library/prefer-explicit-assert": "error",
+    "testing-library/prefer-presence-queries": "error",
+    "testing-library/prefer-screen-queries": "error",
+    "testing-library/prefer-wait-for": "error",
   },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      env: {
+        jest: true,
+      },
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
 };
